880f3043898d3460976fd3e1b9408f7ac1fe4c28
kavinduakash <bkkasamarasingha1220@gmail.com>
2025-11-20 16:59:03 +0530

    update save customer

--- src/main/java/lk/ijse/supermarket/CustomerViewController.java
+++ src/main/java/lk/ijse/supermarket/CustomerViewController.java
@@ -1,6 +1,9 @@
 package lk.ijse.supermarket;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
@@ -21,9 +24,40 @@ public class CustomerViewController{
         String address = customerAddress.getText();
         String salary = customerSalary.getText();
         
-        
-        
-        System.out.println(name + " - " + address + " - " + salary);
+        try {
+       
+            Connection conn = DBConnection.getInstance().getConnection();
+       
+            String sql = "INSERT INTO customer (name, address, salary) VALUES (?,?,?)";
+                
+            PreparedStatement pstm = conn.prepareStatement(sql);
+                
+            pstm.setString(1, name);
+            pstm.setString(2, address);
+            pstm.setDouble(3, Double.parseDouble(salary));
+                
+            int result = pstm.executeUpdate();
+
+            if(result > 0) {
+                System.out.println("Customer saved successfully!");
+                
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Customer Saved!");
+                alert.setHeaderText("Customer saved successfully!");
+                alert.show();
+                
+            } else {
+                System.out.println("Sorry! Something went wrong!");
+                
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Customer Saved!");
+                alert.setHeaderText("Sorry! Something went wrong!");
+                alert.show();
+            }
+            
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
     }
     
 }
