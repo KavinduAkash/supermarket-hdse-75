bc0269df0c884d3144c89d738ec73e5b2c0e4e86
kavinduakash <bkkasamarasingha1220@gmail.com>
2025-11-20 13:57:29 +0530

    update login

--- src/main/java/lk/ijse/supermarket/LoginController.java
+++ src/main/java/lk/ijse/supermarket/LoginController.java
@@ -1,26 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
- */
 package lk.ijse.supermarket;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.fxml.Initializable;
+import java.io.IOException;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
-/**
- * FXML Controller class
- *
- * @author kavinduakash
- */
-public class LoginController implements Initializable {
+public class LoginController {
 
-    /**
-     * Initializes the controller class.
-     */
-    @Override
-    public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+    @FXML
+    private TextField usernameField;
+    
+    @FXML
+    private PasswordField passwordField;
+    
+    @FXML
+    private void login() throws IOException {
+        String realUsername = "KavinduAkash";
+        String realPassword = "Admin@123";
+        
+        String username = usernameField.getText();
+        String password = passwordField.getText();
+        
+        System.out.println(username + " - " + password);
+        
+        if(username.equals(realUsername) & password.equals(realPassword)) {
+        App.setRoot("primary");
+        }
+    }
     
 }
