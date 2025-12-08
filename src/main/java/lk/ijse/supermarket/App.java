405ba5c859f7a2751d00dd20a4e18c9311f8b06d
kavinduakash <bkkasamarasingha1220@gmail.com>
2025-12-08 09:58:22 +0530

    load customer id combobox

--- src/main/java/lk/ijse/supermarket/App.java
+++ src/main/java/lk/ijse/supermarket/App.java
@@ -14,7 +14,8 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("CustomerView"), 640, 480);
+//        scene = new Scene(loadFXML("CustomerView"), 640, 480);
+        scene = new Scene(loadFXML("order"), 600, 400);
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/lk/ijse/supermarket/controller/OrderController.java
+++ src/main/java/lk/ijse/supermarket/controller/OrderController.java
@@ -1,14 +1,46 @@
 package lk.ijse.supermarket.controller;
 
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+import lk.ijse.supermarket.dto.CustomerDTO;
+import lk.ijse.supermarket.model.CustomerModel;
 
 public class OrderController implements Initializable {
 
+    private final CustomerModel customerModel = new CustomerModel();
+    
+    @FXML
+    private ComboBox<Number> comboCustomerId;
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         
+        loadComboCustomerId();
+        
     }    
+    
+    private void loadComboCustomerId() {
+        try {
+        
+            List<CustomerDTO> customerList = customerModel.getAllCustomers();
+            
+            ObservableList<Number> customerIdObList =  FXCollections.observableArrayList();
+            
+            for (CustomerDTO customerDTO : customerList) {
+                customerIdObList.add(customerDTO.getId());
+            }
+            
+            comboCustomerId.setItems(customerIdObList);
+            
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
     
 }
