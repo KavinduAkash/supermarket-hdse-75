bf7a5f423db4e2c0672d0002dccb246c1913d5af
kavinduakash <bkkasamarasingha1220@gmail.com>
2025-12-08 10:22:29 +0530

    load customer data

--- src/main/java/lk/ijse/supermarket/controller/OrderController.java
+++ src/main/java/lk/ijse/supermarket/controller/OrderController.java
@@ -5,9 +5,12 @@ import java.util.List;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
 import lk.ijse.supermarket.dto.CustomerDTO;
 import lk.ijse.supermarket.model.CustomerModel;
 
@@ -17,6 +20,16 @@ public class OrderController implements Initializable {
     
     @FXML
     private ComboBox<Number> comboCustomerId;
+    
+    @FXML
+    private Label lblCustomerAddressValue;
+
+    @FXML
+    private Label lblCustomerNameValue;
+
+    @FXML
+    private Label lblCustomerSalaryValue;
+    
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -41,6 +54,32 @@ public class OrderController implements Initializable {
         } catch(Exception e) {
             e.printStackTrace();
         }
+    }
+    
+    @FXML
+    void handleSelectComboCustomerId(ActionEvent event) {
+
+        try {
+        
+            Number selectedId = comboCustomerId.getSelectionModel().getSelectedItem();
+            int selecteCustomerId = selectedId.intValue();
+
+            CustomerDTO customerDTO = customerModel.searchCustomer(String.valueOf(selecteCustomerId));
+            
+            if(customerDTO!=null) {
+            
+                lblCustomerNameValue.setText(customerDTO.getName());
+                lblCustomerAddressValue.setText(customerDTO.getAddress());
+                lblCustomerSalaryValue.setText(String.valueOf(customerDTO.getSalary()));
+                
+            } else {
+                new Alert(Alert.AlertType.ERROR, "Customer not found!").show();
+            }
+            
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        
     }
     
 }
