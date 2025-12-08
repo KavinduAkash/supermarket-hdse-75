55e0baf68fa2f6879d7812dff4c6eeae0be213a4
kavinduakash <bkkasamarasingha1220@gmail.com>
2025-12-08 10:44:41 +0530

    dev item model

---
+++ src/main/java/lk/ijse/supermarket/model/ItemModel.java
@@ -0,0 +1,48 @@
+package lk.ijse.supermarket.model;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import lk.ijse.supermarket.dto.ItemDTO;
+import lk.ijse.supermarket.util.CrudUtil;
+
+public class ItemModel {
+
+   public List<ItemDTO> getAllItems() throws SQLException {
+    
+        ResultSet rs = CrudUtil.execute("SELECT * FROM item");
+        
+        List<ItemDTO> itemList = new ArrayList<>();
+        
+        while(rs.next()) {
+            int id = rs.getInt("id");
+            String name = rs.getString("name");
+            int qty = rs.getInt("qty");
+            double unitPrice = rs.getDouble("unit_price");
+            
+            itemList.add(new ItemDTO(id, name, qty, unitPrice));
+        }
+        
+        return itemList;
+    }
+   
+   public ItemDTO searchItem(int itemId) throws SQLException {
+    
+        ResultSet rs = CrudUtil.execute("SELECT * FROM item WHERE id=" + itemId);
+        
+        ItemDTO itemDTO = null;
+        
+        while(rs.next()) {
+            int id = rs.getInt("id");
+            String name = rs.getString("name");
+            int qty = rs.getInt("qty");
+            double unitPrice = rs.getDouble("unit_price");
+            
+            itemDTO = new ItemDTO(id, name, qty, unitPrice);
+        }
+        
+        return itemDTO;
+    }
+    
+}
