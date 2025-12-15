4c7af925a6f1c01a0fdf8a9386e92497ee5d5699
kavinduakash <bkkasamarasingha1220@gmail.com>
2025-12-15 11:33:55 +0530

    update report

--- src/main/java/lk/ijse/supermarket/App.java
+++ src/main/java/lk/ijse/supermarket/App.java
@@ -15,7 +15,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 //        scene = new Scene(loadFXML("CustomerView"), 640, 480);
-        scene = new Scene(loadFXML("order"), 600, 400);
+        scene = new Scene(loadFXML("CustomerView"), 600, 400);
         stage.setScene(scene);
         stage.show();
     }
